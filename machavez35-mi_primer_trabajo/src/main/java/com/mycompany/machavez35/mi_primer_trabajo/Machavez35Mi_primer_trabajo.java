dc830ac4c802feaffcf072c243bb108b2113f680
Manuelito2205@ <machavez35@utpl.edu.ec>
2023-10-19 17:26:14 -0500

    este es mi primer programa que subo a mi calse

---
+++ machavez35-mi_primer_trabajo/src/main/java/com/mycompany/machavez35/mi_primer_trabajo/Machavez35Mi_primer_trabajo.java
@@ -0,0 +1,6 @@
+package com.mycompany.machavez35.mi_primer_trabajo;
+public class Machavez35Mi_primer_trabajo {
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+    }
+}
