ac0eab78b2f6c597716dbfa9a753a2ebe043ea7c
Manuel <machavez35@utpl.edu.ec>
2023-10-23 21:08:22 -0500

    MI PRIMER COMENTARIO

---
+++ proyecto1/src/main/java/com/mycompany/proyecto1/Proyecto1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1;
+
+/**
+ *
+ * @author COSAS DE LA Y
+ */
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+      System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+      
+    }
+}
