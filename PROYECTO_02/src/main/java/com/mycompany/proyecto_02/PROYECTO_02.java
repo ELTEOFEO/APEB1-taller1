9255412d1b55f7e047d71886f413e44376be729e
Manuel <machavez35@utpl.edu.ec>
2023-10-24 11:19:14 -0500

    TERCER COMIN TRABAJANDO CON GIT HUB Y GIT CLASROOM

---
+++ PROYECTO_02/src/main/java/com/mycompany/proyecto_02/PROYECTO_02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_02;
+
+/**
+ *
+ * @author COSAS DE LA Y
+ */
+public class PROYECTO_02 {
+
+    public static void main(String[] args) {
+        System.out.println("EJEMPLO 2 BASICO DESDE JAVA VERSIONADO ATRAVEZ DE GIT MANEJADA DESDE GIT HUB ADEMAS SE USAR GIT HUB CLASROOM");
+    }
+}
